7473291b3906ffa168ba15ec04fd9eac26324978
gawaxx <jules.blanc92@gmail.com>
2020-09-05 13:10:08 +0100

    Refactoring code

--- src/Adapters/API.js
+++ src/Adapters/API.js
@@ -1,7 +1,5 @@
 export const API_LINK = (count) => `https://cors-anywhere.herokuapp.com/https://qa-interview-test.qa.splytech.io/api/drivers?latitude=51.5049375&longitude=-0.0964509&count=${count}`
 
-// const jsonify = res => res.json();
-
 const headers = {
     'content-type': 'application/json',
 }
