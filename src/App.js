942534b3208d80a8f4b47a735aa0aa135d276825
gawaxx <jules.blanc92@gmail.com>
2020-09-05 13:21:21 +0100

    Some CSS

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
 
   state = {
     drivers: [],
-    // animation: 'hey', 
     cars: 1,
   }
 
@@ -37,8 +36,6 @@ class App extends Component {
             label='Choose amount of cars to display'
             name='cars to display'
             onChange={this.handleChange}
-            // // options={this.state.options}
-            // value={this.state.animation}
           />
           <Form.Input
             label={`Cars: ${this.state.cars} `}
@@ -62,47 +59,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-// import React, { Component } from 'react';
-// import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
-// import { API, API_LINK } from './Adapters/API'
-
-// const mapStyles = {
-//   width: '100%',
-//   height: '100%'
-// };
-
-// export class MapContainer extends Component {
-
-//   state = {
-//     showingInfoWindow: false,  //Hides or the shows the infoWindow
-//     activeMarker: {},          //Shows the active marker upon click
-//     selectedPlace: {},          //Shows the infoWindow to the selected place upon a marker
-//     drivers: []
-//   };
-
-//   componentDidMount() {
-//     API.GetAPI(API_LINK).then( drivers => this.setState( drivers.drivers ) )
-//   }
-
-//   render() {
-//     return (
-//       <Map
-//         google={this.props.google}
-//         zoom={14}
-//         // key={'AIzaSyBZ8NP6GercRjUxWDD8ugWLYsHDY-7DGL4'}
-//         style={mapStyles}
-//         initialCenter={{
-//          lat: 51.5049375,
-//          lng: -0.0964509
-//         }}
-//       />
-//     );
-//   }
-// }
-
-// export default GoogleApiWrapper({
-//   apiKey: 'AIzaSyBZ8NP6GercRjUxWDD8ugWLYsHDY-7DGL4'
-// })(MapContainer);
